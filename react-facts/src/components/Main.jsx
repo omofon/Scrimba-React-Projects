d5318736d2e0e7fad69b157e98f812be1eddf6d4
Abasiomofon Udoh <omofon.u@gmail.com>
2025-12-22 06:52:28 +0100

    feat(Header): styling done

--- react-facts/src/components/Main.jsx
+++ react-facts/src/components/Main.jsx
@@ -1,16 +1,16 @@
 function Main() {
     return (
-        <main className="container">
-            <h2>Main Section</h2>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </main>
-    )
+      <main className="container">
+        <h1>Fun facts about React</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jordan Walke</li>
+          <li>Has well over 200K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+    );
 }
 
 export default Main
