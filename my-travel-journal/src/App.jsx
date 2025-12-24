393656737280e2c9ab1efdcbeb241238c803817c
Abasiomofon Udoh <omofon.u@gmail.com>
2025-12-24 05:21:20 +0100

    feat(my-travel-journal): getGreeting function

--- my-travel-journal/src/App.jsx
+++ my-travel-journal/src/App.jsx
@@ -1,11 +1,25 @@
 import Header from "./components/common/Header";
 import TravelCard from "./components/travel/TravelCard";
 
+function GetGreeting() {
+  const hours = new Date().getHours();
+
+  if (hours >= 5 && hours < 12) return "morning";
+  if (hours >= 12 && hours < 17) return "afternoon";
+  if (hours >= 17 && hours < 21) return "evening";
+  return "night";
+}
+
 export default function App() {
   return (
     <>
-      <Header />  
+      <Header />
       <TravelCard />
+      <TravelCard />
+      <TravelCard />
+      <TravelCard />
+
+      {/* <p>Hello, it is {GetGreeting()}</p> */}
     </>
   );
 }
