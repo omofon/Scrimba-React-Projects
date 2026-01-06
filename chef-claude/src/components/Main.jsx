ba8559bb4583ef7d498f099cfe538925b7c2321c
Abasiomofon Udoh <omofon.u@gmail.com>
2026-01-06 04:14:13 +0100

    feat(tailwindcss): Main component styling

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,7 +1,19 @@
 function Main() {
-    return (
-        <h2>Hello Main!</h2>
-    )
+  return (
+    <main className="px-4 py-5">
+      <form className="flex justify-center gap-3">
+        <input
+          type="text"
+          aria-label="Add ingredient"
+          placeholder="e.g. oregano"
+          className="border-[#D1D5DB] px-3 py-2 shadow grow min-w-30 max-w-100 rounded-md"
+        />
+        <button className="rounded-md bg-[#141413] text-[#FAFAF8] w-30 border-0 text-sm hover:cursor-pointer">
+          + Add ingredient
+        </button>
+      </form>
+    </main>
+  );
 }
 
-export default Main
+export default Main;
