0b8b15d4999f070548631702fd296dfcb42479bd
Abasiomofon Udoh <omofon.u@gmail.com>
2026-01-06 15:32:56 +0100

    feat(Main.jsx): Pre useState

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -1,17 +1,36 @@
 function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Form submitted!");
+
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+
+    ingredients.push(newIngredient);
+    console.log("New ingredient", ingredients[ingredients.length - 1]);
+  }
+
   return (
-    <main className="px-4 py-5">
-      <form className="flex justify-center gap-3">
+    <main className="px-4 py-5 space-y-5">
+      <form onSubmit={handleSubmit} className="flex justify-center gap-5">
         <input
           type="text"
           aria-label="Add ingredient"
           placeholder="e.g. oregano"
+          name="ingredient"
           className="border-[#D1D5DB] px-3 py-2 shadow grow min-w-30 max-w-100 rounded-md"
         />
         <button className="rounded-md bg-[#141413] text-[#FAFAF8] w-30 border-0 text-sm hover:cursor-pointer">
           + Add ingredient
         </button>
       </form>
+      <ul className="ml-10 list-decimal">
+        {ingredients.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </main>
   );
 }
