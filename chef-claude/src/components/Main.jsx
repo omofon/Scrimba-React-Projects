7a8900f5e7e13b5a81cb33cc0751352700321265
Abasiomofon Udoh <omofon.u@gmail.com>
2026-01-12 09:35:55 +0100

    feat(Main.jsx): Add conditional rendering for generate recipe section

--- chef-claude/src/components/Main.jsx
+++ chef-claude/src/components/Main.jsx
@@ -37,32 +37,31 @@ function Main() {
       {ingredients.length > 0 && (
         <section>
           <h2 className="text-xl font-bold mb-2">Ingredients on hand:</h2>
-          <ul className="ml-10 list-disc" aria-live="polite">
+          <ul className="ml-10 list-disc mb-12" aria-live="polite">
             {ingredientsListItems}
           </ul>
-          <div className="flex justify-between items-center rounded-lg bg-[#F0EFEB] py-2.5 px-7">
-            <div>
-              <h3 className="text-lg font-medium leading-6">
-                Ready for a recipe?
-              </h3>
-              <p className="text-[#6B7290] text-sm leading-5">
-                Generate a recipe from your list of ingredients.
-              </p>
+          {ingredients.length > 3 && (
+            <div className="flex flex-col md:flex-row justify-between items-start md:items-center rounded-lg bg-[#F0EFEB] py-2.5 px-7">
+              <div className="space-y-2">
+                <h3 className="text-lg font-medium">Ready for a recipe?</h3>
+                <p className="text-[#6B7290] text-sm">
+                  Generate a recipe from your list of ingredients.
+                </p>
+              </div>
+              <button className="rounded-md bg-[#D17557] shadow text-[#FAFAF8] px-4 py-2 text-sm cursor-pointer">
+                Get a recipe
+              </button>
             </div>
-            <button className="rounded-md bg-[#D17557] shadow text-[#FAFAF8] px-4 py-2 text-sm cursor-pointer">
-              Get a recipe
-            </button>
-          </div>
+          )}
+          {/* Clear Ingredients */}
+          <button
+            onClick={clearList}
+            className="px-5 py-1 rounded-xl bg-[#141413] text-[#FAFAF8] cursor-pointer hover:bg-gray-700 mt-10"
+          >
+            Clear
+          </button>
         </section>
       )}
-
-      {/* Clear Ingredients */}
-      <button
-        onClick={clearList}
-        className="px-5 py-1 rounded-xl bg-[#141413] text-[#FAFAF8] cursor-pointer hover:bg-gray-700"
-      >
-        Clear
-      </button>
     </main>
   );
 }
