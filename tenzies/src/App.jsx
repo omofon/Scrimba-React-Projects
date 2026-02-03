c58d523ab9e4bcd054f9924421933ee318cf1a78
Abasiomofon Udoh <omofon.u@gmail.com>
2026-02-03 07:58:00 +0100

    feat: styling for text content

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -44,6 +44,13 @@ function App() {
   ));
   return (
     <main>
+      <section className="tenzies-section">
+        <h1>Tenzies</h1>
+        <p>
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
+      </section>
       <div className="dice-container">{diceElements}</div>
       <button onClick={rollDice} className="roll-dice">
         Roll
