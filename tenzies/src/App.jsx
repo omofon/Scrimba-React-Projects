6813218a9ee3d3f110c525c268d022767f583e33
Abasiomofon Udoh <omofon.u@gmail.com>
2026-02-03 06:25:36 +0100

    feat: create state for dice

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,7 +1,29 @@
-function App(){
-    return <main>
-        <div>Heloo world!</div>
+import { useState } from "react";
+import Die from "./components/Die";
+
+function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+    }));
+  }
+
+  function rollDice() {
+    setDice(generateAllNewDice());
+  }
+
+  const diceElements = dice.map((num, id) => <Die key={id} value={num} />);
+  return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button onClick={rollDice} className="roll-dice">
+        Roll
+      </button>
     </main>
+  );
 }
 
-export default App
+export default App;
